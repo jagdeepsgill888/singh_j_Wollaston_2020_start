a1b942b390bab4ed97ac37d7e8657bd1cf2d3db1
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-07-16 12:03:33 -0400

    adding menu function

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(() => {
+	//"use strict";
+
+	console.log("fired");
+
+	let button = document.querySelector("#button");
+	    burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+
+})();
